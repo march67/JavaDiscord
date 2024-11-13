9f79672fb59d813727d7a0d60d9e1b3c8b3fc9e9
Quentin Doniczka <doniczka.quentin67@gmail.com>
2024-11-13 14:37:22 +0100

    fix

--- client/src/main/java/com/javadiscordprojet/Main.java
+++ client/src/main/java/com/javadiscordprojet/Main.java
@@ -2,6 +2,6 @@ package com.javadiscordprojet;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World back");
+        System.out.println("Hello World client");
     }
 }
