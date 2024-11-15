e0390f6e1598ab18137357001beec80d2d767447
quentin doniczka <doniczka.quentin67@gmail.com>
2024-11-15 22:26:39 +0100

    fix/pom.xml

--- client/src/main/java/com/javadiscordprojet/Main.java
+++ client/src/main/java/com/javadiscordprojet/Main.java
@@ -1,7 +1,16 @@
 package com.javadiscordprojet;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("JavaFX Window");
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World client");
+        launch(args);
     }
 }
