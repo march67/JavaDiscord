a44792b9a1ae137d187b43f025075ae24a813341
march67 <td.ta.david@gmail.com>
2024-12-09 16:12:32 +0100

    chore: docker/postgresql/init.sql modified to handle register and one-to-one message operations

--- docker/postgresql/init.sql
+++ docker/postgresql/init.sql
@@ -1,10 +1,28 @@
 -- Schéma relationnel
-CREATE TABLE test (
-    id SERIAL PRIMARY KEY,
-    testName VARCHAR(50) NOT NULL
+CREATE TABLE Application_User (
+ application_user_id VARCHAR(50) PRIMARY KEY,
+ email VARCHAR(50) NOT NULL,
+ username VARCHAR(50) NOT NULL,
+ password VARCHAR(50) NOT NULL
 );
 
--- Données de seed
-INSERT INTO test (testName) VALUES 
-    ('test1'),
-    ('test2');
+CREATE TABLE Conversation (
+ conversation_id VARCHAR(50) PRIMARY KEY,
+ application_user_id VARCHAR(50) NOT NULL REFERENCES Application_User(application_user_id)
+);
+
+CREATE TABLE ConversationParticipant (
+ application_user_id VARCHAR(50),
+ conversation_id VARCHAR(50),
+ PRIMARY KEY (application_user_id, conversation_id),
+ FOREIGN KEY (application_user_id) REFERENCES Application_User(application_user_id),
+ FOREIGN KEY (conversation_id) REFERENCES Conversation(conversation_id)
+);
+
+CREATE TABLE Message (
+ message_id VARCHAR(50) PRIMARY KEY,
+ application_user_id VARCHAR(50) REFERENCES Application_User(application_user_id),
+ message_content VARCHAR(500) NOT NULL,
+ created_at TIMESTAMP NOT NULL,
+ conversation_id VARCHAR(50) NOT NULL REFERENCES Conversation(conversation_id)
+);
