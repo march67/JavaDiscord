2213505b6f44cb5058785e15a42c0261b407fdf6
march67 <td.ta.david@gmail.com>
2024-12-11 14:07:12 +0100

    feature: use UUID instead of string for keys

--- docker/postgresql/init.sql
+++ docker/postgresql/init.sql
@@ -1,28 +1,28 @@
 -- Schéma relationnel
 CREATE TABLE Application_User (
- application_user_id VARCHAR(50) PRIMARY KEY,
+ application_user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
  email VARCHAR(50) NOT NULL,
  username VARCHAR(50) NOT NULL,
  password VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Conversation (
- conversation_id VARCHAR(50) PRIMARY KEY,
- application_user_id VARCHAR(50) NOT NULL REFERENCES Application_User(application_user_id)
+ conversation_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+ application_user_id UUID NOT NULL REFERENCES Application_User(application_user_id)
 );
 
 CREATE TABLE ConversationParticipant (
- application_user_id VARCHAR(50),
- conversation_id VARCHAR(50),
+ application_user_id UUID,
+ conversation_id UUID,
  PRIMARY KEY (application_user_id, conversation_id),
  FOREIGN KEY (application_user_id) REFERENCES Application_User(application_user_id),
  FOREIGN KEY (conversation_id) REFERENCES Conversation(conversation_id)
 );
 
 CREATE TABLE Message (
- message_id VARCHAR(50) PRIMARY KEY,
- application_user_id VARCHAR(50) REFERENCES Application_User(application_user_id),
+ message_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+ application_user_id UUID REFERENCES Application_User(application_user_id),
+  conversation_id UUID NOT NULL REFERENCES Conversation(conversation_id),
  message_content VARCHAR(500) NOT NULL,
- created_at TIMESTAMP NOT NULL,
- conversation_id VARCHAR(50) NOT NULL REFERENCES Conversation(conversation_id)
+ created_at TIMESTAMP NOT NULL
 );
